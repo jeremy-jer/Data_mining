c89e597f7d1a2b22d28e03388e37c6ba1ff7087a
jeremy-jer <jeremy.sellam@efrei.net>
2024-07-18 12:16:32 +0200

    1 and 2

--- projet.py
+++ projet.py
@@ -1,39 +1,78 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from sklearn.impute import KNNImputer
 
-# Titre de l'application
-st.title('Data Mining')
-st.subheader('Analyse de données')
-# Importation des données
-df = pd.read_csv('data.csv')
+def convert_columns(df):
+    for col in df.columns:
+        # Essayez de convertir les colonnes numériques qui sont lues comme des objets en int ou float
+        try:
+            df[col] = pd.to_numeric(df[col])
+        except ValueError:
+            pass
+    return df
 
-# Affichage des données
-st.write("Données importées :")
-st.write(df)
-st.write('-------------------------------------------------------------------------------------------------------------')
+def main():
+    st.title("Data mining")
 
-# Analyse des données
-# Première ligne
-st.write("5 premières lignes :")
-st.write(df.head())
-st.write('-------------------------------------------------------------------------------------------------------------')
+    # Section pour charger le fichier CSV
+    st.subheader("Chargement des données")
+    file = st.file_uploader("Uploader un fichier CSV", type=["csv"])
 
-# Dernière ligne
-st.write("5 dernières lignes :")
-st.write(df.tail())
-st.write('-------------------------------------------------------------------------------------------------------------')
+    st.sidebar.header('Menu')
+    partie = st.sidebar.radio("Titre des parties", ('Data Pre-processing and Cleaning', 'Visualization of the cleaned data', 'Clustering or Prediction', 'Learning Evaluation'))    
+    
+    df = None 
+    
+    # Chargement des données si un fichier est téléchargé
+    if file is not None:
+        try:
+            df = pd.read_csv(file, on_bad_lines='skip', sep=';')
+            st.success("Fichier chargé avec succès")
+            df = convert_columns(df)
+        except pd.errors.ParserError as e:
+            st.error(f"Erreur lors de la lecture du fichier: {e}")
+        except Exception as e:
+            st.error(f"Une erreur est survenue: {e}")
+    
+    if df is not None:
+        if partie == 'Data Pre-processing and Cleaning':
+            st.subheader("Aperçu des données")
+            st.markdown("*Premières lignes du jeu de données :*")
+            st.write(df.head())
+            st.markdown("*Dernières lignes du jeu de données :*")
+            st.write(df.tail())
 
-# Noms de colonnes
-st.write("Noms des colonnes :")
-st.write(df.columns)
+            st.subheader("Étude statistique des données")
+            st.write("Nombre total de lignes :", df.shape[0])
+            st.write("Nombre total de colonnes :", df.shape[1])
+            st.write("Liste des colonnes :", df.columns.tolist())
+            st.write("Nombre de valeurs manquantes par colonne :")
+            st.write(df.isnull().sum())
 
-# Nombre de lignes et de colonnes
-st.write("Nombre de lignes et de colonnes :")
-st.write(df.shape)
+            st.write("Description de la moyenne, la médiane, l'écart-type.... ")
+            st.write(df.describe())
 
-# Nombre de valeurs nulles
-st.write("Nombre de valeurs nulles :")
-st.write(df.isnull().sum())
+        elif partie == 'Visualization of the cleaned data':
+            st.write("Quelle opération voulez-vous faire ? ")
+            choix = st.selectbox('Choix', ['Supprimer les lignes', 'Supprimer les colonnes', 'Remplacer les valeurs nulles', 'Imputer par KNN'])
 
+            if choix == 'Supprimer les lignes':
+                st.write("Suppression de lignes")
+                df = df.dropna()
+                st.write(df.head())
+            elif choix == 'Supprimer les colonnes':
+                st.write("Suppression de colonnes")
+                df = df.dropna(axis=1)
+                st.write(df.head())
+            elif choix == 'Remplacer les valeurs nulles par la moyenne':
+                st.write("Remplacement des valeurs nulles")
+                df = df.fillna(df.mean())
+                st.write(df.head())
+            elif choix == 'Remplacer par KNN':
+                st.write("KNN")
+                Knn = KNNImputer(n_neighbors=5)
+                df[:] = Knn.fit_transform(df)
+                st.write(df.head())
 
+if __name__ == "__main__":
+    main()
